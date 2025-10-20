5af23ea045433d428a08def6c575130406bd2f10
testuser <test@test.test>
2025-10-20 09:20:09 +0300

    added file 'function.js', added intSequence script

---
+++ function.js
@@ -0,0 +1,20 @@
+{let intSequence = [5,8]
+for(i = intSequence[0]; i <=intSequence[1]; i++){
+console.log(i);
+}
+}
+console.log("---");
+
+
+{let intSequence = [1,10]
+for(i = intSequence[0]; i <=intSequence[1]; i++){
+console.log(i);
+}
+}
+console.log("---");
+
+{let intSequence = [7,16]
+for(i = intSequence[0]; i <=intSequence[1]; i++){
+console.log(i);
+}
+}
