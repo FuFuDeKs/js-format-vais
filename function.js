189d1bd8201026ab66c60f33b8f8daf2bb18323c
testuser <test@test.test>
2025-10-20 09:31:20 +0300

    updated file 'function.js', now is prettier

--- function.js
+++ function.js
@@ -1,20 +1,23 @@
-{let intSequence = [5,8]
-for(i = intSequence[0]; i <=intSequence[1]; i++){
-console.log(i);
-}
-}
-console.log("---");
-
-
-{let intSequence = [1,10]
-for(i = intSequence[0]; i <=intSequence[1]; i++){
-console.log(i);
-}
+{
+    let intSequence = [5, 8]
+    for (i = intSequence[0]; i <= intSequence[1]; i++) {
+        console.log(i);
+    }
 }
 console.log("---");
 
-{let intSequence = [7,16]
-for(i = intSequence[0]; i <=intSequence[1]; i++){
-console.log(i);
+
+{
+    let intSequence = [1, 10]
+    for (i = intSequence[0]; i <= intSequence[1]; i++) {
+        console.log(i);
+    }
 }
+console.log("---");
+
+{
+    let intSequence = [7, 16]
+    for (i = intSequence[0]; i <= intSequence[1]; i++) {
+        console.log(i);
+    }
 }
